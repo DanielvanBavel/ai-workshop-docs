e5b8a9af79b86ea96f59939f8503fe0d36bda0aa
Daniel ccnl <daniel.vanbavel@codecentric.nl>
2023-03-02 10:57:03 +0100

    reset files

--- AI-workshop/docusaurus.config.js
+++ AI-workshop/docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'Build your own application with TDD and chatGPT',
   tagline: 'AI Workshop - 30.03.2023',
   url: 'https://the-experts.nl',
-  baseUrl: '/ai-workshop-docs/',
+  baseUrl: '/ai-workshop-docs/AI-workshop',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
